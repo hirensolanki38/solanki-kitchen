0ef9d9f87c42af8397c5d0c96c9a7de8eda7c74b
Hiren Solanki <hiren.solanki@borngroup.com>
2026-01-21 17:29:44 +0530

    Food App : Filter Logic Added

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,11 +3,18 @@ import resList from "../utils/resList";
 import { useState } from "react";
 
 const Body = () => {
+  let resListData = resList;
     return (
         <div className="body">
-            <div className="search">Search</div>
+            <div className="filter">
+              <button className="filter-btn" onClick={() => {
+                console.log(resListData)
+                resListData = resListData.filter((res) => res.info.avgRating > 4.3)
+                console.log(resListData);
+              }}>Top Rated Restaurnt</button>
+            </div>
             <div className="res-container">
-              {resList.map((restaurant, index) => (
+              {resListData.map((restaurant, index) => (
                 <RestaurntCard key={restaurant.info.id} resData={restaurant} />
               ))}
             </div>
