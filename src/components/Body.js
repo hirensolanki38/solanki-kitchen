769384d0306e52f3e63bb303c0dc8db39b3e8487
Hiren Solanki <hiren.solanki@borngroup.com>
2026-02-02 20:29:55 +0530

    Food App : Live API call, Optional Chaining, Conditional Rendering

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,29 @@
 import RestaurntCard from "./RestaurntCard";
 import resList from "../utils/resList";
-import { useState } from "react";
+import { useState,useEffect } from "react";
+import Shimmer from "./Shimmer";
 
 const Body = () => {
   // let resListData = resList;
-  const [resListData, setResListData] = useState(resList);
-    return (
+  // const [resListData, setResListData] = useState(resList);
+  const [resListData, setResListData] = useState([]);
+    useEffect(() => {
+      fetchData();
+    }, []);
+
+    const fetchData = async () => {
+      console.log('Fetch Data function');
+      // https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.2072943&lng=72.8436443&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING
+
+      const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.2072943&lng=72.8436443&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+
+      const json = await data.json();
+      // Optional Chaining
+      setResListData(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    }
+
+    // Conditional Rendering
+    return resListData.length === 0 ? <Shimmer /> : (
         <div className="body">
             <div className="filter">
               <button className="filter-btn" onClick={() => {
