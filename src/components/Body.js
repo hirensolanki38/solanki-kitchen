f2c8fc4471957ed30ebd8358a5b60db6040a1b39
Hiren Solanki <hiren.solanki@borngroup.com>
2026-02-04 19:34:13 +0530

    Food App : Search Functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,45 +1,84 @@
 import RestaurntCard from "./RestaurntCard";
 import resList from "../utils/resList";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
 
 const Body = () => {
   // let resListData = resList;
   // const [resListData, setResListData] = useState(resList);
   const [resListData, setResListData] = useState([]);
-    useEffect(() => {
-      fetchData();
-    }, []);
+  const [filterResList, setFilterResList] = useState([]);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    const fetchData = async () => {
-      console.log('Fetch Data function');
-      // https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.2072943&lng=72.8436443&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING
+  const [searchText, setSearchText] = useState("");
 
-      const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.2072943&lng=72.8436443&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+  // console.log("Body Rendered");
 
-      const json = await data.json();
-      // Optional Chaining
-      setResListData(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-    }
+  const fetchData = async () => {
+    console.log("Fetch Data function");
+    // https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.2072943&lng=72.8436443&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING
 
-    // Conditional Rendering
-    return resListData.length === 0 ? <Shimmer /> : (
-        <div className="body">
-            <div className="filter">
-              <button className="filter-btn" onClick={() => {
-                console.log(resListData)
-                const filteredResListData  = resListData.filter((res) => res.info.avgRating > 4.3)
-                setResListData(filteredResListData);
-                console.log(resListData);
-              }}>Top Rated Restaurnt</button>
-            </div>
-            <div className="res-container">
-              {resListData.map((restaurant, index) => (
-                <RestaurntCard key={restaurant.info.id} resData={restaurant} />
-              ))}
-            </div>
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.2072943&lng=72.8436443&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING",
+    );
+
+    const json = await data.json();
+    // Optional Chaining
+    setResListData(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants,
+    );
+    setFilterResList(resListData);
+  };
+
+  // Conditional Rendering
+  return resListData.length === 0 ? (
+    <Shimmer />
+  ) : (
+    <div className="body">
+      <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            className="search-btn"
+            onClick={() => {
+              const filteredResListData = resListData.filter((res) =>
+                res.info.name.toLowerCase().includes(searchText.toLowerCase()),
+              );
+              setFilterResList(filteredResListData);
+            }}
+          >
+            Search
+          </button>
         </div>
-    )
+
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredResListData = filterResList.filter(
+              (res) => res.info.avgRating > 4.3,
+            );
+            setFilterResList(filteredResListData);
+          }}
+        >
+          Top Rated Restaurnt
+        </button>
+      </div>
+      <div className="res-container">
+        {filterResList.map((restaurant, index) => (
+          <RestaurntCard key={restaurant.info.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Body;
